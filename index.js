ce3b75f526950acb362beeefc9d050e0809b9bc4
shaikdilshad <shaikdilshad968@gmail.com>
2026-05-12 06:57:47 +0530

    update cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000' }));
+app.use(cors({ origin: [process.env.FRONTEND_URL || 'http://localhost:3000', 'https://outpro-india-sand.vercel.app/'] }));
 app.use(express.json());
 
 // Database Connection
